964d9ba09984323dffbeaacf66c5d8d3b9858d14
Dylan Strickler <dylanstrickler@Peters-iMac.local>
2022-03-26 13:12:54 -0400

    Initial commit

---
+++ src/main/java/woodenlalaloo/treesplus/Treesplus.java
@@ -0,0 +1,10 @@
+package woodenlalaloo.treesplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Treesplus implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
